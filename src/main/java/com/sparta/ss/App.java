dfb9a889a353d2555676531b514c34e1ab66ca43
MattWVile <matt.vile@yahoo.co.uk>
2022-03-28 16:59:23 +0100

    made a file to print information in the config file

--- src/main/java/com/sparta/ss/App.java
+++ src/main/java/com/sparta/ss/App.java
@@ -1,13 +1,15 @@
 package com.sparta.ss;
 
+import com.sparta.ss.view.PrintOut;
+
 /**
  * Hello world!
  *
  */
-//public class App
-//{
-//    public static void main( String[] args )
-//    {
-//        System.out.println( "Hello World!" );
-//    }
-//}
+public class App
+{
+    public static void main( String[] args )
+    {
+        PrintOut.printOut();
+    }
+}
