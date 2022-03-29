0779f3fabd6d5bdb1dccbe654f05b6be7ca64340
MattWVile <matt.vile@yahoo.co.uk>
2022-03-29 17:06:50 +0100

    added tests for trainee allocator

--- src/test/java/com/sparta/ss/MonthlyIteratorTest.java
+++ src/test/java/com/sparta/ss/MonthlyIteratorTest.java
@@ -1,16 +1,34 @@
 package com.sparta.ss;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MonthlyIteratorTest {
+
+    @BeforeAll
+    static void createListCenters() {
+        MonthIterator.monthIterator();
+    }
+
     @Test
     @DisplayName("Check waiting list is updated")
     void checkWaitingListIsUpdated() {
-        MonthIterator.traineeAllocator();
-        assertEquals(true,MonthIterator.getWaitingList()>=50 && MonthIterator.getWaitingList()<=100);
+        assertEquals(true,MonthIterator.getWaitingList()!=0);
+    }
+
+    @Test
+    @DisplayName("check centres are generated")
+    void checkCentresAreGenerated() {
+        assertEquals(6,TrainingCenterManager.getTotCenters());
+    }
+
+    @Test
+    @DisplayName("check centres are filled")
+    void checkCentresAreFilled() {
+        assertEquals(true,TrainingCenterManager.getFullCenters() >= 1);
     }
 }
