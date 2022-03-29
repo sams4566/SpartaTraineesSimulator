7dd30bf7d8fbbe822fce524621838d9c5d64842a
Gowrimurali <>
2022-03-29 10:39:56 +0100

    Adding arraylist and waiting list

--- src/main/java/com/sparta/ss/MonthIterator.java
+++ src/main/java/com/sparta/ss/MonthIterator.java
@@ -4,16 +4,22 @@ import com.sparta.ss.view.PrintOut;
 
 public class MonthIterator {
 
+    int waitingList = 0;
+
     public void monthIterator(){
         PrintOut.printOut();
         for(int i = 0; i < getProperty("amountOfTimesToRun"); i++){
             for(int j = 1; j <= getProperty("amountOfYears")*12; j++){
+                int numberofTrainees = RandomGenerator.getRandom();
                 if(j%2 != 1){
                     for(int t = 0; t < getProperty("amountOfCentresGeneratedEveryOtherMonth");t++){
-                        TrainingCenter trainingCenter = new TrainingCenter(100,0);
+                        TrainingCenter trainingCenter = new TrainingCenter();
                     }
-                    
                 }
+                if(TrainingCenter.openTrainingCenter.size()==0){
+                    waitingList+= numberofTrainees;
+                }
+                //else
             }
         }
 
