9365037702064de8b5a8631b2bae2520cfe3badd
Gowrimurali <>
2022-03-29 11:18:51 +0100

    dding waiting test file

--- src/main/java/com/sparta/ss/MonthIterator.java
+++ src/main/java/com/sparta/ss/MonthIterator.java
@@ -4,25 +4,29 @@ import com.sparta.ss.view.PrintOut;
 
 public class MonthIterator {
 
-    int waitingList = 0;
+    public static int waitingList = 0;
 
     public void monthIterator(){
         PrintOut.printOut();
         for(int i = 0; i < getProperty("amountOfTimesToRun"); i++){
             for(int j = 1; j <= getProperty("amountOfYears")*12; j++){
-                int numberofTrainees = RandomGenerator.getRandom();
                 if(j%2 != 1){
                     for(int t = 0; t < getProperty("amountOfCentresGeneratedEveryOtherMonth");t++){
                         TrainingCenter trainingCenter = new TrainingCenter();
                     }
                 }
-                if(TrainingCenter.openTrainingCenter.size()==0){
-                    waitingList+= numberofTrainees;
-                }
-                //else
+                traineeAllocator();
             }
         }
 
+    }
+
+    public void traineeAllocator() {
+        int numberofTrainees = RandomGenerator.getRandom();
+        if(TrainingCenter.openTrainingCenter.size()==0){
+            waitingList+= numberofTrainees;
+        }
+        //else
     }
 
     private int getProperty(String property){
