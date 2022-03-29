a7a390c2955e1851ae20d3cbdb9259d1a14cc339
Simone Conigliaro <sconigliaro@spartaglobal.com>
2022-03-29 17:45:00 +0100

    cleaning code

--- src/main/java/com/sparta/ss/ConvertCSVFile.java
+++ src/main/java/com/sparta/ss/ConvertCSVFile.java
@@ -7,6 +7,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ConvertCSVFile {
+
+    private static final String FILENAME = "src/main/resources/output";
+    private static final String CSV_EXT = ".csv";
+
     public static void createCVSFile(int openCenters, int fullCenters, int traineesInTraining, int waitingList) {
 
         String[] headers = new String[]{"Open centers", "Full centers", "Trainees currently training", "Trainees on the waiting list"};
@@ -27,11 +31,11 @@ public class ConvertCSVFile {
 
     public static void createFile(List<String[]> records) {
         int numFile = 1;
-        String fileName = "src/main/resources/output" + numFile + ".csv";
+        String fileName = FILENAME + numFile + CSV_EXT;
 
         while (checkFileExists(fileName)) {
             numFile++;
-            fileName = "src/main/resources/output" + numFile + ".csv";
+            fileName = FILENAME + numFile + CSV_EXT;
         }
         try (CSVWriter writer = new CSVWriter(new FileWriter(fileName))) {
             writer.writeAll(records);
