8dcb330c17882bb925337d4aa9234f0bd69eff8f
mujm4tic <mmoosavi@hotmail.co.uk>
2022-03-29 12:24:01 +0100

    allocation phase 1 done, testing in progress

--- src/main/java/com/sparta/ss/TrainingCenter.java
+++ src/main/java/com/sparta/ss/TrainingCenter.java
@@ -23,9 +23,28 @@ public class TrainingCenter {
     }
 
 
-    private void allocatetrainees(){
-            // require random generator
+    private void allocateTrainees(int numberTrainees){
+        //just need to randomize the numberTrainees we assign to each center.
+        // Probably something in else break. Maybe use list of randomly generated ints instead of numberTrainees?
+        for (TrainingCenter centre : openTrainingCenter) {
+            if (numberTrainees > 0){
+                if (numberTrainees + centre.occupiedSeats < 100) {
+                    centre.occupiedSeats += numberTrainees;
+                    numberTrainees = 0;
+                } else if (numberTrainees + centre.occupiedSeats == 100) {
+                    centre.occupiedSeats += numberTrainees;
+                    numberTrainees = 0;
+                    centre.isOpen = false;
+                } else {
+                    numberTrainees = numberTrainees - centre.vacancySeats;
+                    centre.occupiedSeats = 100;
+                    centre.isOpen = false;
+                }
+            } else {
+                break;
+            }
         }
+    }
 
 
         public void checkVacancy(){
